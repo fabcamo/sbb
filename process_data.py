ed98d0cd1e6a3158a14e4f2d392df51146e48d6f
fabcamo <fab.camo@gmail.com>
2025-04-23 18:20:56 +0200

    Fixed some errors in the ependes data

--- process_data.py
+++ process_data.py
@@ -8,30 +8,19 @@ from geolib_plus.robertson_cpt_interpretation import RobertsonCptInterpretation
 from geolib_plus.robertson_cpt_interpretation import UnitWeightMethod, ShearWaveVelocityMethod, OCRMethod
 
 
-# TO LOAD THE PICKLE FILE FOR BAVOIS
-bavois_pickle = r"c:\Users\camposmo\OneDrive - Stichting Deltares\Desktop\Geotechnical site investigations\L210_22400-26400_Bavois site investigation\CPT\cpt_bavois.pkl"
-with open(bavois_pickle, "rb") as f:
-    cpt_bavois = pickle.load(f)
-
-# TO LOAD THE PICKLE FILE FOR CHAVORNAY
-chavornay_pickle = r"c:\Users\camposmo\OneDrive - Stichting Deltares\Desktop\Geotechnical site investigations\L210_28200-33000_Chavornay-Ependes site investigation\CPT\cpt_chavornay.pkl"
-with open(chavornay_pickle, "rb") as f:
-    cpt_chavornay = pickle.load(f)
-
-
-#random_cpt_path = r"C:\Users\camposmo\Stichting Deltares\RESET - Documents\Activities year 4\2a - Strength modeling\data\cpt\CPT000000217362_IMBRO.gef"
-# cpt = GefCpt()
-# cpt.read(random_cpt_path)
-
-
-# Load the CSV with additional CPT information
-metadata_path = r"c:\Users\camposmo\OneDrive - Stichting Deltares\Desktop\Geotechnical site investigations\cpt_other_details.csv"
-metadata_df = pd.read_csv(metadata_path)
-
-print(metadata_df.columns)
-
-
 def create_cpt_object(cpt_name: str, cpt_dict: dict, metadata_df: pd.DataFrame, site_name: str) -> GefCpt:
+    """
+    Create a GefCpt object and populate it with data from the cpt_dict and metadata_df.
+
+    params:
+        cpt_name (str): The name of the CPT.
+        cpt_dict (dict): Dictionary containing CPT data.
+        metadata_df (pd.DataFrame): DataFrame containing metadata information.
+        site_name (str): The name of the site.
+
+    returns:
+        GefCpt: An instance of the GefCpt class populated with data.
+    """
     cpt = GefCpt()
 
     data = cpt_dict[cpt_name]
@@ -62,6 +51,17 @@ def create_cpt_object(cpt_name: str, cpt_dict: dict, metadata_df: pd.DataFrame, 
 
 
 def initialize_cpt_objects(cpt_dict: dict, metadata_df: pd.DataFrame, site_name: str) -> list:
+    """
+    Initializes all the CPT objects in the given dictionary and populates them using the craete_cpt_object function.
+
+    params:
+        cpt_dict (dict): Dictionary containing CPT data.
+        metadata_df (pd.DataFrame): DataFrame containing metadata information.
+        site_name (str): The name of the site.
+
+    returns:
+        list: A list of initialized GefCpt objects.
+    """
     cpt_objects = []
     for cpt_name in cpt_dict:
         try:
@@ -73,15 +73,31 @@ def initialize_cpt_objects(cpt_dict: dict, metadata_df: pd.DataFrame, site_name:
     return cpt_objects
 
 
+# Load the pickle files with the CPT data
+bavois_pickle = r"c:\Users\camposmo\OneDrive - Stichting Deltares\Desktop\Geotechnical site investigations\L210_22400-26400_Bavois site investigation\CPT\cpt_bavois.pkl"
+with open(bavois_pickle, "rb") as f:
+    cpt_bavois = pickle.load(f)
+
+chavornay_pickle = r"c:\Users\camposmo\OneDrive - Stichting Deltares\Desktop\Geotechnical site investigations\L210_28200-33000_Chavornay-Ependes site investigation\CPT\cpt_chavornay.pkl"
+with open(chavornay_pickle, "rb") as f:
+    cpt_chavornay = pickle.load(f)
+
+ependes_pickle = r"c:\Users\camposmo\OneDrive - Stichting Deltares\Desktop\Geotechnical site investigations\L210_33000-37500_Ependes-Yverdon site investigation\CPT Roh-Daten\cpt_ependes.pkl"
+with open(ependes_pickle, "rb") as f:
+    cpt_ependes = pickle.load(f)
+
+# Load the metadata for the CPTs (I manually built this)
+metadata_path = r"c:\Users\camposmo\OneDrive - Stichting Deltares\Desktop\Geotechnical site investigations\cpt_other_details.csv"
+metadata_df = pd.read_csv(metadata_path)
+
+# random_cpt_path = r"C:\Users\camposmo\Stichting Deltares\RESET - Documents\Activities year 4\2a - Strength modeling\data\sotec\north\24SP1888_DKMP057.GEF"
+# cpt = GefCpt()
+# cpt.read(random_cpt_path)
+
+# Initialize and populate the CPT objects
 cpt_bavois_list = initialize_cpt_objects(cpt_bavois, metadata_df, site_name="bavois")
 cpt_chavornay_list = initialize_cpt_objects(cpt_chavornay, metadata_df, site_name="chavornay")
-
-
-print(cpt_bavois_list)
-
-
-
-
+cpt_ependes_list = initialize_cpt_objects(cpt_ependes, metadata_df, site_name="ependes")
 
 # Define the DGeoLib+ interpreter conditions
 interpreter = RobertsonCptInterpretation()
@@ -95,31 +111,38 @@ for cpt in cpt_bavois_list:
     cpt.pre_process_data()
     cpt.interpret_cpt(interpreter)
 
+for cpt in cpt_chavornay_list:
+    # pre-process the CPT
+    cpt.pre_process_data()
+    cpt.interpret_cpt(interpreter)
 
-    # Interpret the CPT
-print(cpt_bavois_list)
+for cpt in cpt_ependes_list:
+    # pre-process the CPT
+    cpt.pre_process_data()
+    cpt.interpret_cpt(interpreter)
 
-
-
-cpt = cpt_bavois_list[0]  # Select the first CPT for demonstration
+cpt = cpt_chavornay_list[1]  # Select the first CPT for demonstration
 
 # Plot in the same space the effective stress and total stress in x vs depth in y
 plt.figure(figsize=(5, 10))
 plt.plot(cpt.effective_stress, cpt.depth, label='Effective Stress', color='darkred')
 plt.plot(cpt.total_stress, cpt.depth, label='Total Stress', color='black')
 plt.plot(cpt.hydro_pore_pressure, cpt.depth, label='Pore Pressure', color='darkblue')
-#plt.plot(cpt.pore_pressure_u2*1000, cpt.depth, label='Pore Pressure U2', alpha=0.4, color='grey')
+plt.plot(cpt.pore_pressure_u2 * 1000, cpt.depth, label='Pore Pressure U2', alpha=0.4, color='grey')
 plt.xlabel('Stress (kPa)')
 plt.ylabel('Depth NAP (m)')
 plt.title(f'Total and effective stress - CPT{cpt.name}')
 plt.legend()
 plt.grid(True)
 plt.gca().invert_yaxis()  # Invert y-axis to have depth increasing downwards
-
-
-
+# limit x from 0 to 200
+plt.xlim(-5, 200)
 
 plt.show()
+
+
+
+
 
 
 
@@ -165,18 +188,6 @@ plt.show()
 #
 #
 # cpt.pre_process_data()
-
-
-print(cpt)
-
-
-
-
-
-
-
-
-
 
 
 # # Loop over each CPT and generate plots
