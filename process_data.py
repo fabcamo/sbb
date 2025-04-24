62f99283b313c09692dc18cf8fac320e2c06dff0
fabcamo <fab.camo@gmail.com>
2025-04-24 06:09:37 +0200

    Some progress on the results, need to check units and formulas

--- process_data.py
+++ process_data.py
@@ -73,6 +73,77 @@ def initialize_cpt_objects(cpt_dict: dict, metadata_df: pd.DataFrame, site_name:
     return cpt_objects
 
 
+def save_results_as_csv(cpt, cpt_dict, results_path):
+    """
+    After manually populating the CPT object, pre-procesing and interpreting the data, this function
+    gathers all the relevant information and saves it as a CSV file for easy access
+
+    Params:
+        cpt (GefCpt): The CPT object with the interpreted data.
+        cpt_dict (dict): The dictionary containing the CPT data that is read from the pickle file.
+        results_path (str): The path where the CSV file will be saved.
+
+    Returns:
+        None
+    """
+
+    cpt_key = cpt.name.split("_")[0]  # Extract CPTU01 from CPTU01_bavois
+    qn = (cpt.qt - cpt.effective_stress)/1000
+    # if any number inside the qn array is negative, set it to 0
+    qn[qn < 0] = 0
+
+    Bq = (cpt.pore_pressure_u2 - cpt.hydro_pore_pressure / 1000) / (cpt.qt / 1000 - cpt.effective_stress / 1000)
+    Nkt_a = 10.5 + 7 * np.log(cpt.Fr)
+    Nkt_b = 10.5 - 4.6 * np.log(Bq + 0.1)
+
+
+    # Create a DataFrame with the interpreted values
+    interpreted_data = {
+        'Depth* (m)': cpt.depth,
+        'Depth to reference level (m)': cpt.depth_to_reference,
+
+        'qc* (MPa)': cpt.tip,
+        'fs* (MPa)': cpt.friction,
+        'Rf* (%)': cpt.friction_nbr,
+        'Fr (%)': cpt.Fr,
+
+        'PWP u2* (MPa)': cpt.pore_pressure_u2,
+        'PWP u0 (MPa)': cpt.hydro_pore_pressure/1000,
+
+        'Effective Stress (kPa)': cpt.effective_stress,
+        'Total Stress (kPa)': cpt.total_stress,
+
+        'qt (MPa)': cpt.qt/1000,
+        'qn (MPa)': qn,
+        'Qtn (MPa)': cpt.Qtn/1000,
+
+        'Bq* (MPa)': cpt_dict.get(cpt_key, {}).get('Bq', np.nan),
+
+        'Bq (MPa)': Bq,
+
+        'E0 (MPa)': cpt.E0,
+        'G0 (MPa)': cpt.G0,
+        'IC ': cpt.IC,
+        'lithology': cpt.lithology,
+        'poisson:': cpt.poisson,
+        'psi': cpt.psi,
+        'relative density': cpt.relative_density,
+        'rho (kg/m3)': cpt.rho,
+        'Vs (m/s)': cpt.vs,
+
+        'Nkt_a': Nkt_a,
+        'Nkt_b': Nkt_b,
+
+        'St* (-):': cpt_dict.get(cpt_key, {}).get('St', np.nan),
+        'St (-)': ((cpt.qt - cpt.total_stress)/Nkt_a)*(1/cpt.friction/1000)
+
+    }
+    interpreted_df = pd.DataFrame(interpreted_data)
+    # Save the DataFrame as a CSV file
+    interpreted_df.to_csv(f"{results_path}/{cpt.name}_interpreted.csv", index=False)
+
+
+
 # Load the pickle files with the CPT data
 bavois_pickle = r"c:\Users\camposmo\OneDrive - Stichting Deltares\Desktop\Geotechnical site investigations\L210_22400-26400_Bavois site investigation\CPT\cpt_bavois.pkl"
 with open(bavois_pickle, "rb") as f:
@@ -89,6 +160,9 @@ with open(ependes_pickle, "rb") as f:
 # Load the metadata for the CPTs (I manually built this)
 metadata_path = r"c:\Users\camposmo\OneDrive - Stichting Deltares\Desktop\Geotechnical site investigations\cpt_other_details.csv"
 metadata_df = pd.read_csv(metadata_path)
+
+# Save results path
+results_path = r"c:\Users\camposmo\OneDrive - Stichting Deltares\Desktop\Geotechnical site investigations"
 
 # random_cpt_path = r"C:\Users\camposmo\Stichting Deltares\RESET - Documents\Activities year 4\2a - Strength modeling\data\sotec\north\24SP1888_DKMP057.GEF"
 # cpt = GefCpt()
@@ -109,7 +183,11 @@ interpreter.user_defined_water_level = True
 for cpt in cpt_bavois_list:
     # pre-process the CPT
     cpt.pre_process_data()
+    # Run the interpretation
     cpt.interpret_cpt(interpreter)
+    # Save the results as a CSV file
+    save_results_as_csv(cpt, cpt_bavois, results_path)
+
 
 for cpt in cpt_chavornay_list:
     # pre-process the CPT
@@ -121,24 +199,6 @@ for cpt in cpt_ependes_list:
     cpt.pre_process_data()
     cpt.interpret_cpt(interpreter)
 
-cpt = cpt_chavornay_list[1]  # Select the first CPT for demonstration
-
-# Plot in the same space the effective stress and total stress in x vs depth in y
-plt.figure(figsize=(5, 10))
-plt.plot(cpt.effective_stress, cpt.depth, label='Effective Stress', color='darkred')
-plt.plot(cpt.total_stress, cpt.depth, label='Total Stress', color='black')
-plt.plot(cpt.hydro_pore_pressure, cpt.depth, label='Pore Pressure', color='darkblue')
-plt.plot(cpt.pore_pressure_u2 * 1000, cpt.depth, label='Pore Pressure U2', alpha=0.4, color='grey')
-plt.xlabel('Stress (kPa)')
-plt.ylabel('Depth NAP (m)')
-plt.title(f'Total and effective stress - CPT{cpt.name}')
-plt.legend()
-plt.grid(True)
-plt.gca().invert_yaxis()  # Invert y-axis to have depth increasing downwards
-# limit x from 0 to 200
-plt.xlim(-5, 200)
-
-plt.show()
 
 
 
@@ -156,12 +216,25 @@ plt.show()
 
 
 
-
-
-
-
-
-
+# cpt = cpt_chavornay_list[1]  # Select the first CPT for demonstration
+#
+# # Plot in the same space the effective stress and total stress in x vs depth in y
+# plt.figure(figsize=(5, 10))
+# plt.plot(cpt.effective_stress, cpt.depth, label='Effective Stress', color='darkred')
+# plt.plot(cpt.total_stress, cpt.depth, label='Total Stress', color='black')
+# plt.plot(cpt.hydro_pore_pressure, cpt.depth, label='Pore Pressure', color='darkblue')
+# plt.plot(cpt.pore_pressure_u2 * 1000, cpt.depth, label='Pore Pressure U2', alpha=0.4, color='grey')
+# plt.xlabel('Stress (kPa)')
+# plt.ylabel('Depth NAP (m)')
+# plt.title(f'Total and effective stress - CPT{cpt.name}')
+# plt.legend()
+# plt.grid(True)
+# plt.gca().invert_yaxis()  # Invert y-axis to have depth increasing downwards
+# # limit x from 0 to 200
+# plt.xlim(-5, 200)
+#
+# plt.show()
+#
 
 
 # # Create manually the objects for the CPT CPTU01 in bavois
@@ -182,11 +255,7 @@ plt.show()
 # cpt.coordinates = [row["E"].item(), row["N"].item()] if not row.empty else [None, None]
 # cpt.local_reference_level = -row["elev_cpt"].item() if not row.empty else None
 #
-#
-#
-#
-#
-#
+##
 # cpt.pre_process_data()
 
 
